f859a0a6541a641a08d5e849756ae29e5d9fa543
selçuk güler <2003022022@student.beykent.edu.tr>
2022-01-17 15:24:20 +0300

    Heroku db settings

--- models/database.js
+++ models/database.js
@@ -1,8 +1,14 @@
 const mysql = require('mysql');
 
+// exports.connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '1234',
+// 	database: 'quizz-app',
+// });
 exports.connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '1234',
-	database: 'quizz-app',
+	host: "eu-cdbr-west-02.cleardb.net",
+	user: "b51719bc817438",
+	password: "ecddcbdc",
+	database: "heroku_7894ab0bc917673",
 });
